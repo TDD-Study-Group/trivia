58d4799f6882f3f55135744bc3c7244425fbb79c
ULLXM <me@you.de>
2020-11-17 11:02:33 +0100

    "mob next [ci-skip]"

--- kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeTest.kt
+++ kotlin/src/test/kotlin/com/adaptionsoft/games/trivia/SomeTest.kt
@@ -4,110 +4,32 @@ import com.adaptionsoft.games.trivia.runner.GameRunner
 import com.adaptionsoft.games.uglytrivia.Game
 import com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut
 import org.approvaltests.Approvals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.util.*
 
 class SomeTest {
-    @Test
-    fun true_is_true() {
-        assertTrue(true)
-    }
-
     private val rand = Random(42)
-
-    @Test
-    fun EXPLORE() {
-        val captured = tapSystemOut {
-            val aGame = Game()
-
-            aGame.add("Chet")
-            aGame.add("Pat")
-            aGame.add("Sue")
-
-
-            do {
-
-                aGame.roll(rand.nextInt(5) + 1)
-
-                if (rand.nextInt(9) == 7) {
-                    GameRunner.notAWinner = aGame.wrongAnswer()
-                } else {
-                    GameRunner.notAWinner = aGame.wasCorrectlyAnswered()
-                }
-
-            } while (GameRunner.notAWinner)
-        }
-        Approvals.verify(captured)
-    }
-
-    @Test
-    fun gameWithOnePlayer() {
-        val captured = tapSystemOut {
-            val aGame = Game()
-
-            aGame.add("Chet")
-
-            do {
-
-                aGame.roll(rand.nextInt(5) + 1)
-
-                if (rand.nextInt(9) == 7) {
-                    GameRunner.notAWinner = aGame.wrongAnswer()
-                } else {
-                    GameRunner.notAWinner = aGame.wasCorrectlyAnswered()
-                }
-
-            } while (GameRunner.notAWinner)
-        }
-        Approvals.verify(captured)
-    }
-
-    @Test
-    fun gameWithTwoPlayer() {
-        val captured = tapSystemOut {
-            val aGame = Game()
-
-            aGame.add("Whatever")
-            aGame.add("Chet")
-
-            do {
-
-                aGame.roll(rand.nextInt(5) + 1)
-
-                if (rand.nextInt(9) == 7) {
-                    GameRunner.notAWinner = aGame.wrongAnswer()
-                } else {
-                    GameRunner.notAWinner = aGame.wasCorrectlyAnswered()
-                }
-
-            } while (GameRunner.notAWinner)
-        }
-        Approvals.verify(captured)
-    }
 
     @Test
     fun gameWithRandomPlayer() {
         val captured = tapSystemOut {
-            repeat(1000){
+            repeat(5000) {
                 val aGame = Game().apply {
-                    val nextInt = rand.nextInt(10)
-                    (0..nextInt).forEach { i -> add("Name $i") }
+                    val playerCount = rand.nextInt(6)
+                    if(playerCount != 0){
+                        (1..playerCount).forEach { i -> add("Name $i") }
+                    }
                 }
-
                 do {
-
                     aGame.roll(rand.nextInt(5) + 1)
-
                     if (rand.nextInt(9) == 7) {
                         GameRunner.notAWinner = aGame.wrongAnswer()
                     } else {
                         GameRunner.notAWinner = aGame.wasCorrectlyAnswered()
                     }
-
                 } while (GameRunner.notAWinner)
-            }        }
+            }
+        }
         Approvals.verify(captured)
     }
-
 }
